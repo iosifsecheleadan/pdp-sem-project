97b2affe1dbf2749c7fb42a0267813bf41c87859
AvramPop <daniel.avram.pop@gmail.com>
2020-12-11 17:27:44 +0200

    should see if this works

--- src/main/java/com/example/puzzle/Main.java
+++ src/main/java/com/example/puzzle/Main.java
@@ -3,38 +3,52 @@ package com.example.puzzle;
 import com.example.puzzle.domain.Puzzle;
 import com.example.puzzle.domain.PuzzleException;
 
-import java.util.Scanner;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) {
-        play(4);
-        System.out.println("Bye!");
-    }
+  public static void main(String[] args) {
+    play(4);
+    System.out.println("Bye!");
+  }
 
-    private static void play(int size) {
-        Puzzle puzzle = new Puzzle(size, true);
-        System.out.println("""
-                Try to get to the result using commands "up" | "down" | "left" | "right"
-                The first letter of either word will suffice (u, d, l, r)
-                Type "exit" to quit
-                Here's what that should look like:\s
-                """ + new Puzzle(size, true).toString());
-        Scanner input = new Scanner(System.in);
-
-        while (! puzzle.isSolution()) {
-            System.out.printf("Your puzzle\n%s\n>", puzzle.toString());
-            String line = input.nextLine();
-            char command = line.charAt(0);
-            try {
-                switch (command) {
-                    case 'u' -> puzzle.shift(Puzzle.Shift.UP);
-                    case 'd' -> puzzle.shift(Puzzle.Shift.DOWN);
-                    case 'l' -> puzzle.shift(Puzzle.Shift.LEFT);
-                    case 'r' -> puzzle.shift(Puzzle.Shift.RIGHT);
-                    case 'e' -> { return; }
-                    default -> System.out.printf("Wrong input: \"%s\" \n", line);
-                }
-            } catch (PuzzleException e) { System.out.printf("Can't shift %s \n", line); } }
-        System.out.printf("You did it! Good Job! \n%s", puzzle.toString());
+  private static void play(int size) {
+//    Puzzle puzzle = new Puzzle(size, true);
+    //        System.out.println(
+    //                "Try to get to the result using commands \"up\" | \"down\" | \"left\" |
+    // \"right\" " +
+    //                "The first letter of either word will suffice (u, d, l, r)" +
+    //                "Type \"exit\" to quit" +
+    //                "Here's what that should look like:\n" +
+    //                new Puzzle(size, true).toString());
+    //        Scanner input = new Scanner(System.in);
+    //
+    //        while (! puzzle.isSolution()) {
+    //            System.out.printf("Your puzzle\n%s\n>", puzzle.toString());
+    //            String line = input.nextLine();
+    //            char command = line.charAt(0);
+    //            try {
+    //                switch (command) {
+    //                    case 'u' -> puzzle.shift(Puzzle.Shift.UP);
+    //                    case 'd' -> puzzle.shift(Puzzle.Shift.DOWN);
+    //                    case 'l' -> puzzle.shift(Puzzle.Shift.LEFT);
+    //                    case 'r' -> puzzle.shift(Puzzle.Shift.RIGHT);
+    //                    case 'e' -> { return; }
+    //                    default -> System.out.printf("Wrong input: \"%s\" \n", line);
+    //                }
+    //            } catch (PuzzleException e) { System.out.printf("Can't shift %s \n", line); } }
+    //        System.out.printf("You did it! Good Job! \n%s", puzzle.toString());
+//    int board[][] = new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    Puzzle puzzle = new Puzzle(4);
+    puzzle.setParent(null);
+    Solver solver = new Solver(4);
+//    System.out.println("Starting from:");
+//    System.out.println(puzzle.toString());
+    try {
+      Puzzle solution = solver.findSolution(puzzle);
+      System.out.println(solution.toString());
+    } catch (PuzzleException | ExecutionException | InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 }
--- src/main/java/com/example/puzzle/Solver.java
+++ src/main/java/com/example/puzzle/Solver.java
@@ -14,7 +14,6 @@ import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 public class Solver {
-  private final Stack<Puzzle> solution = new Stack<>();
   private final int FOUND = -1;
   private final ExecutorService executorService;
   private final int INFINITY = Integer.MAX_VALUE;
@@ -23,66 +22,63 @@ public class Solver {
     this.executorService = Executors.newFixedThreadPool(numberOfThreads);
   }
 
-  public List<Puzzle> findSolution(Puzzle root) throws PuzzleException, ExecutionException, InterruptedException {
+  public Puzzle findSolution(Puzzle root) throws PuzzleException, ExecutionException, InterruptedException {
     int bound = root.getHeuristics();
-    solution.push(root);
     boolean found = false;
-    AbstractMap.SimpleEntry<Integer, Puzzle> searchAnswer;
+    AbstractMap.SimpleEntry<Integer, Puzzle> searchAnswer = null;
     while (!found) {
       searchAnswer = search(root, 0, bound);
       if (searchAnswer.getKey() == FOUND) {
         found = true;
       }
-      if (searchAnswer.getKey() == -2) {
+      if (searchAnswer.getKey() == INFINITY) {
         throw new PuzzleException();
       }
       bound = searchAnswer.getKey();
     }
-    return solution;
+    executorService.shutdown();
+    return searchAnswer.getValue();
   }
 
   private AbstractMap.SimpleEntry<Integer, Puzzle> search(Puzzle current, int currentCost, int bound) throws ExecutionException, InterruptedException {
-    Puzzle node = solution.get(solution.size() - 1);
-    int totalCost = currentCost + node.getHeuristics();
+    int totalCost = currentCost + current.getHeuristics();
     if (totalCost > bound) {
       return new AbstractMap.SimpleEntry<>(totalCost, current);
     }
-    if (node.isSolution()) {
+    if (current.isSolution()) {
       return new AbstractMap.SimpleEntry<>(FOUND, current);
     }
     int min = INFINITY;
 
-    List<Puzzle> successors = successorsSanitized(node);
+    List<Puzzle> successors = successorsSanitized(current);
     List<Future<AbstractMap.SimpleEntry<Integer, Puzzle>>> tasks = new ArrayList<>();
-    for (Puzzle next : successors) {
-      Future<AbstractMap.SimpleEntry<Integer, Puzzle>> future = executorService.submit(() -> search(next, currentCost + 1, bound));
+    for(int i = 0; i < successors.size(); i++) {
+      Puzzle temp = successors.get(i);
+      Future<AbstractMap.SimpleEntry<Integer, Puzzle>> future = executorService.submit(() -> search(temp, currentCost + 1, bound));
       tasks.add(future);
     }
-    for (Future<AbstractMap.SimpleEntry<Integer, Puzzle>> future : tasks) {
-      AbstractMap.SimpleEntry<Integer, Puzzle> result = future.get();
-      if (solution.search(result.getValue()) != -1) {
-        solution.push(result.getValue());
-        if (result.getKey() == FOUND) {
-          return new AbstractMap.SimpleEntry<>(totalCost, current);
-        }
-        if (result.getKey() < min) {
-          min = result.getKey();
-        }
-        solution.pop();
-      }
 
+    for (int i = 0; i < tasks.size(); i++) {
+      AbstractMap.SimpleEntry<Integer, Puzzle> result = tasks.get(i).get();
+      if (result.getKey() == FOUND) {
+        return new AbstractMap.SimpleEntry<>(FOUND, result.getValue());
+      }
+      if (result.getKey() < min) {
+        min = result.getKey();
+      }
     }
     return new AbstractMap.SimpleEntry<>(min, current);
   }
 
   private List<Puzzle> successorsSanitized(Puzzle node) {
     List<Puzzle> successors = node.successors();
-    if (solution.size() > 1) {
-      successors =
-          successors.stream()
-              .filter(s -> !s.equals(solution.get(solution.size() - 2)))
-              .collect(Collectors.toList());
-    }
+    successors.forEach(s -> s.setParent(node));
+//    if (solution.size() > 1) {
+//      successors =
+//          successors.stream()
+//              .filter(s -> !s.equals(solution.get(solution.size() - 2)))
+//              .collect(Collectors.toList());
+//    }
 
     return successors;
   }
